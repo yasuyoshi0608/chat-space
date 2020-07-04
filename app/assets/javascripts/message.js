19885246e1ecaceeec258575eba37435766cee30
yasuyoshi0608 <i>
2020-07-04 16:24:59 +0900

    edit comment

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,35 +1,37 @@
 $(function(){
+
       function buildHTML(message){
         if ( message.image ) {
           var html =
-          `<div class="content">
-                <div class="user-date">
-                  <div class="user-date__user-name">
-                    ${message.user_name}
-                  </div>
-                  <div class="user-date__message-time">
-                    ${message.created_at}
-                  </div>
-                </div>
-                  <div class="message-talk">
-                    <p class="message-talk__content">
-                      ${message.content}
-                    </p>
-                  </div>
-              <img src=${message.image} >
-          </div>`
-          return html;
-        } else {
-          var html =
-          `<div class="content">
-                <div class="user-date">
-                  <div class="user-date__user-name">
-                  ${message.user_name}
-                </div>
-                <div class="user-date__message-time">
-                  ${message.created_at}
-                </div>
+          `<div class="content" data-message-id=${message.id}>
+            <div class="user-date">
+              <div class="user-date__user-name">
+                ${message.user_name}
               </div>
+              <div class="user-date__message-time">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-talk">
+              <p class="message-talk__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        //同様にdata-idが反映されるようにしている
+        var html =
+          `<div class="content" data-message-id=${message.id}>
+            <div class="user-date">
+              <div class="user-date__user-name">
+                ${message.user_name}
+              </div>
+              <div class="user-date__message-time">
+                ${message.created_at}
+              </div>
+            </div>
             <div class="message-talk">
               <p class="message-talk__content">
                 ${message.content}
@@ -66,4 +68,35 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.content:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.chat-main-contents').append(insertHTML);
+      $('.chat-main-contents').animate({ scrollTop: $('.chat-main-contents')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
       resources :messages, only: :index, defaults: { format: 'json' }
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
